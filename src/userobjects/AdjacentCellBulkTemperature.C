c68c561f4a70712ef39e3ef6c205e2bb7569f97c
Matthew Falcone <mapfalcone@gmail.com>
2026-05-21 13:17:21 +0100

    Add passing tests for adjacent cell user object

--- src/userobjects/AdjacentCellBulkTemperature.C
+++ src/userobjects/AdjacentCellBulkTemperature.C
@@ -3,11 +3,17 @@
 #include "FoamProblem.h"
 #include "HippoInterface.h"
 #include "MooseTypes.h"
+#include "petsclog.h"
 #include <ListOps.H>
+#include <Pstream/mpi/PstreamGlobals.H>
+#include <PstreamReduceOps.H>
+#include <UList.H>
 #include <VectorSpace.H>
 #include <fvMesh.H>
+#include <ops.H>
 #include <scalar.H>
 #include <scalarField.H>
+#include "Pstream.H"
 
 registerMooseObject("hippoApp", AdjacentCellBulkTemperature);
 
@@ -20,42 +26,38 @@ AdjacentCellBulkTemperature::validParams()
 }
 
 AdjacentCellBulkTemperature::AdjacentCellBulkTemperature(const InputParameters & params)
-  : GeneralUserObject(params), HippoInterface(this), _T_bulk{getTBoundaryField()}
+  : GeneralUserObject(params),
+    HippoInterface(this),
+    _foam_patch{getFoamPatch(getParam<SubdomainName>("boundary"))}
 {
 }
 
-const Foam::scalarField &
-AdjacentCellBulkTemperature::getTBoundaryField()
+const Foam::fvPatch &
+AdjacentCellBulkTemperature::getFoamPatch(const std::string & boundary)
 {
-  const auto & T{getFvMesh().lookupObject<Foam::volScalarField>("T")};
-  const Foam::label & boundaryId{
-      getFvMesh().boundary().findIndex(getParam<SubdomainName>("boundary"))};
-  return T.boundaryField()[boundaryId].patchInternalField();
+  const Foam::label & boundaryId{getFvMesh().boundary().findIndex(boundary)};
+  return getFvMesh().boundary()[boundaryId];
 }
 
 Real
 AdjacentCellBulkTemperature::spatialValue(const Point & point) const
 {
-  const Foam::label & boundaryId{
-      getFvMesh().boundary().findIndex(getParam<SubdomainName>("boundary"))};
-
-  const Foam::fvPatch & patch = getFvMesh().boundary()[boundaryId];
-
   // Boundary face centres
-  const Foam::vectorField & face_centres = patch.Cf();
+  const Foam::vectorField & face_centres = _foam_patch.Cf();
   Foam::point p_moose{point(0), point(1), point(2)};
   Foam::scalar minDist = Foam::rootVGreat;
   Foam::label idx;
+
   for (int i = 0; i < face_centres.size(); ++i)
   {
     Foam::point p = face_centres[i];
     Foam::scalar dist{Foam::magSqr(p_moose - p)};
     if (dist < minDist)
     {
-      idx = i;
+      idx = _foam_patch.faceCells()[i];
       minDist = dist;
     }
   }
 
-  return _T_bulk[idx];
+  return getFvMesh().lookupObject<Foam::volScalarField>("T")[idx];
 }
