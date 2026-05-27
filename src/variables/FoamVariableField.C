6ad0cefeaaaabb3e9b74b30be7dcedcf64275378
Matthew Falcone <mapfalcone@gmail.com>
2026-05-27 11:30:20 +0100

    Add tests for FoamHeatTransferCoeff

--- src/variables/FoamVariableField.C
+++ src/variables/FoamVariableField.C
@@ -1,8 +1,9 @@
 #include "FoamFieldBase.h"
+#include "FoamProblem.h"
 #include "FoamVariableField.h"
 #include "InputParameters.h"
 #include "MooseTypes.h"
-#include "FoamProblem.h"
+#include "hippoUtils.h"
 
 registerMooseObject("hippoApp", FoamVariableField);
 
@@ -30,19 +31,9 @@ FoamVariableField::transferVariable()
   // Loop through subdomains extracting foam_variable and setting on libMesh elements
   for (auto subdomain : getFoamMesh().getSubdomainList())
   {
-    size_t patch_count = getFoamMesh().getPatchCount(subdomain);
-    size_t patch_offset = getFoamMesh().getPatchOffset(subdomain);
-
     auto & var = getFvMesh().boundary()[subdomain].lookupPatchField<Foam::volScalarField, double>(
         _foam_variable);
-    for (size_t j = 0; j < patch_count; ++j)
-    {
-      auto elem = patch_offset + j;
-      auto elem_ptr = getFoamMesh().getElemPtr(elem + getFoamMesh().rank_element_offset);
-      assert(elem_ptr);
-      auto dof_t = elem_ptr->dof_number(moose_var.sys().number(), moose_var.number(), 0);
-      moose_var.sys().solution().set(dof_t, var[j]);
-    }
+    Hippo::internal::copyFieldFoamToMoose(getFoamMesh(), var, moose_var, subdomain);
   }
 
   moose_var.sys().solution().close();
